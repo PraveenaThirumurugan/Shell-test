6e36f0ac24d5646cc8e43208fc9f1b6890f6440f
PraveenaThirumurugan <137968153+PraveenaThirumurugan@users.noreply.github.com>
2023-09-04 21:28:01 +0530

    Update software-installed-verify1.ps1

--- software-installed-verify1.ps1
+++ software-installed-verify1.ps1
@@ -1,10 +1,1 @@
-Start-Transcript -Path "C:\install\automation\logs\install.log" - Force
-
-$softwareName='notepad'
-
-#if ( ( Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -like "$softwareName*"){
-    Write-Output "$softwareName Software is already installed"
-    Write-Host "--------> Exiting <--------" -ForegroundColor Red -BackgroundColor Yellow
-    
-
-Stop-Transcript
+Hello World!
